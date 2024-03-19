8b0d00c21d27552e7f8c6b53a4210bf3e24061d5
QingyangZeng-121 <126250059+QingyangZeng-121@users.noreply.github.com>
2024-03-19 23:14:58 +0000

    Update sparrow_test.py

--- src/test/sparrow_test.py
+++ src/test/sparrow_test.py
@@ -1,26 +1,37 @@
 import unittest
 import sys
 
-from sparrow_extraction import get_manufacture_model, get_identification_number_list
+from sparrow_extraction import get_manufacture_model, get_identification_number_list,get_identification_parts_list,extract_sparrow_pdf
+
 sys.path.append("..")
+
 
 class TestSparrowExtraction(unittest.TestCase):
     def test_get_manufacture_model(self):
         # Provide a specific description that matches your data
         description = "Chain Block 6mtr HOL"
-        expected_manufacturer, expected_model = "Tiger", "TCB11"
+        expected_manufacturer, expected_model = "", ""
         manufacturer, model = get_manufacture_model(description)
         self.assertEqual(manufacturer, expected_manufacturer)
         self.assertEqual(model, expected_model)
 
     def test_get_identification_number_list(self):
         # Test case for a specific identification number pattern
-        identification_numbers = "S101392"
-        expected_output = ["S101392"]
-        output = get_identification_number_list(identification_numbers, 12)
+        identification_numbers = "D971-1 to MGL1"
+        expected_output = ["D971-1", "D971-2", "D971-3"]
+        output = get_identification_number_list(identification_numbers, 3)
         self.assertEqual(output, expected_output)
+    def test_get_identification_parts_list(self):
+        result = get_identification_parts_list("1", 4)
+        self.assertEqual(["1", "2", "3", "4"], result)
+
+    def test_extract_sparrow_pdf(self):
+        pdf_path = "../resources/centurion.pdf"
+        result = extract_sparrow_pdf(pdf_path)
+        self.assertEqual(None, result)
 
     # You can add more test methods for other functions or scenarios
+
 
 if __name__ == '__main__':
     unittest.main()
